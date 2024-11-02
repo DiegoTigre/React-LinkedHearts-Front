c99c69b2a59dd5b89d98df3a38fa85050bac4b2a
Diego Rivera <soportepci.1990@gmail.com>
2024-11-02 02:09:50 -0600

    version 1.1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,32 +9,40 @@ export const Footer = () => {
     backgroundColor: 'white', 
     width: '100%', 
     height: '100px',
-    marginTop: '40px'
+    marginTop: '80px'
     
     }}>
     <div style={{
         backgroundColor: 'white', 
         width: '100%', 
-        height: '100px',
+        height: '150px',
         textAlign: 'center',
         paddingTop: '15px',
         color: 'black'
+        
         }}>
 
     
    
     <text >Contactanos </text>
     <br></br>
-    
-    <text >Asesor de Ventas Lic. Daniel Minor Cel. 844 138 66 11</text>
     <br></br>
-    <text >Soporte Tecnico Ing. Diego Rivera Cel. 813 871 60 16  </text>
+    
+    <text >Asesor de Ventas Lic.</text> 
+    <br></br>
+    <text>Daniel Minor Cel. 844 138 66 11</text>
+    <br></br>
+    <br></br>
+    <text >Soporte Tecnico </text> 
+    <br></br>
+    <text>Ing. Diego Rivera Cel. 813 871 60 16  </text>
     <br></br>
     <text >email: soportepci.1990@gmail.com</text>
+    
 
     
     </div>
-    <div style={{backgroundColor:'#060302', height:'30px',textAlign:'center', }}>
+    <div style={{backgroundColor:'#060302', height:'40px',textAlign:'center', }}>
     <text style={{color:'white', }}>© 2024 Copyright: MICORA</text>
     </div>
 
